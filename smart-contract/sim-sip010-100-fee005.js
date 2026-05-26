9fd8d7062b023593057ae4d41255a07e5580aff5
distributed-nerd <shaaibusuleiman20@gmail.com>
2026-05-26 07:25:48 +0100

    feat(sim): build sim-sip010-100-fee005.js chunk 1/17 — part 54/70

---
+++ smart-contract/sim-sip010-100-fee005.js
@@ -0,0 +1,12 @@
+const {
+  makeContractCall,
+  AnchorMode,
+  PostConditionMode,
+  broadcastTransaction,
+  uintCV,
+  principalCV,
+  noneCV,
+} = require('@stacks/transactions');
+const { STACKS_MAINNET } = require('@stacks/network');
+const fetch = require('node-fetch');
+const fs = require('fs');
