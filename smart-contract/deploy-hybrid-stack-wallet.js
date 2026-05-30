cafb2298fd6b7d2c7d9ec97e927c37dd89ec023c
distributed-nerd <shaaibusuleiman20@gmail.com>
2026-05-30 08:24:18 +0100

    feat(deploy-hybrid): introduce const API (part 236/400)

--- smart-contract/deploy-hybrid-stack-wallet.js
+++ smart-contract/deploy-hybrid-stack-wallet.js
@@ -13,3 +13,6 @@ const fs = require('fs');
 const CONTRACT_NAME = 'hybrid-stack-wallet';
 const CONTRACT_FILE = './contracts/hybrid-stack-wallet.clar';
 const DEPLOY_FEE = 200000n;
+const API = 'https://api.hiro.so';
+const EXPECTED_ADDRESS = process.env.STACKS_EXPECTED_ADDRESS;
+
