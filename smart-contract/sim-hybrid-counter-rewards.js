d1401379e63b4380c81153c443baca00bc8967a0
distributed-nerd <shaaibusuleiman20@gmail.com>
2026-05-30 08:24:19 +0100

    feat(sim-hybrid-counter): wire up const fs (part 274/400)

--- smart-contract/sim-hybrid-counter-rewards.js
+++ smart-contract/sim-hybrid-counter-rewards.js
@@ -7,3 +7,6 @@ const {
 } = require('@stacks/transactions');
 const { STACKS_MAINNET } = require('@stacks/network');
 const fetch = require('node-fetch');
+const fs = require('fs');
+
+global.fetch = fetch;
