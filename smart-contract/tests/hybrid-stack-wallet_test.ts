0f09dfad7098e99d39fa744c4d9ffd0b8c4c4fed
distributed-nerd <shaaibusuleiman20@gmail.com>
2026-05-30 08:24:18 +0100

    feat(hybrid-stack-wallet-tests): expand const tokenContract (part 188/400)

--- smart-contract/tests/hybrid-stack-wallet_test.ts
+++ smart-contract/tests/hybrid-stack-wallet_test.ts
@@ -176,3 +176,6 @@ Clarinet.test({
   name: "decrement-with-burn: also burns and bumps tally",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     const deployer = accounts.get('deployer')!;
+    const tokenContract = `${deployer.address}.sip010-token`;
+    initializeHybrid(chain, deployer);
+    const block = chain.mineBlock([
