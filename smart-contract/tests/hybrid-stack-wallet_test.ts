c9d6b9a7f1eb37e2d4d2db8d6e158d51b6bbcd3f
distributed-nerd <shaaibusuleiman20@gmail.com>
2026-05-30 08:24:18 +0100

    feat(hybrid-stack-wallet-tests): build out lines 216-218 (part 200/400)

--- smart-contract/tests/hybrid-stack-wallet_test.ts
+++ smart-contract/tests/hybrid-stack-wallet_test.ts
@@ -213,3 +213,6 @@ Clarinet.test({
     const deployer = accounts.get('deployer')!;
     initializeHybrid(chain, deployer);
     chain.mineBlock([
+      Tx.contractCall('hybrid-stack-wallet', 'claim-proposal-reward',
+        [types.uint(1)], deployer.address),
+      Tx.contractCall('hybrid-stack-wallet', 'claim-proposal-reward',
