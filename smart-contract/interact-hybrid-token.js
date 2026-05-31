2b2d3e80938a3509918396e8516a620b6cf7daa0
cryptolucks <luckxz001@gmail.com>
2026-05-31 09:52:05 +0100

    feat(hybrid-token-contract): add API and contract constants (part 123/175)

--- smart-contract/interact-hybrid-token.js
+++ smart-contract/interact-hybrid-token.js
@@ -6,3 +6,11 @@
  *   node interact-hybrid-token.js <command> [args...]
  *
  * Commands:
+ *   initialize
+ *   set-paused <true|false>
+ *   mint-to <amount> <recipient>
+ *   burn-from <amount>
+ *   approve <spender> <amount>
+ *   deposit-to-pool <wallet-id> <amount>
+ *   withdraw-from-pool <wallet-id> <amount>
+ *   stake-tokens <amount> <lock-blocks>
